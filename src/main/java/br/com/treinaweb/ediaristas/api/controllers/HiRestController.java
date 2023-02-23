f76d208b753de2baa3f5127182251193b4dfdc6c
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-23 17:33:26 -0300

    :test_tube: test controller

---
+++ src/main/java/br/com/treinaweb/ediaristas/api/controllers/HiRestController.java
@@ -0,0 +1,23 @@
+package br.com.treinaweb.ediaristas.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HiRestController {
+
+	@GetMapping
+	public Map<String, String> hello(){
+		var json =  new HashMap<String, String>();
+		json.put("message", "Hello");
+
+		return json;
+	}
+}
