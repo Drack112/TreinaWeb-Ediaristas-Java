d211bc9c11b4b198a8613589d05f35d7e3e55031
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-23 17:32:47 -0300

    :thread: new models of foto and update usuario model

--- src/main/java/br/com/treinaweb/ediaristas/core/models/Usuario.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/models/Usuario.java
@@ -1,19 +1,14 @@
 package br.com.treinaweb.ediaristas.core.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -41,5 +36,28 @@ public class Usuario {
     @Column(name = "tipo_usuario", length = 8, nullable = false)
     @Enumerated(EnumType.STRING)
     private TipoUsuario tipoUsuario;
-    
+
+	@Column(nullable = true, length = 11, unique = true)
+	private String cpf;
+
+	@Column(nullable = true)
+	private LocalDate nascimento;
+
+	@Column(nullable = true, length = 11)
+	private String telefone;
+
+	@Column(nullable = true)
+	private Double reputacao;
+
+	@Column(name = "chave_pix", nullable = true, unique = true)
+	private String chavePix;
+
+	@OneToOne(fetch = FetchType.LAZY, orphanRemoval = true)
+	@JoinColumn(name = "foto_documento", nullable = true)
+	private Foto fotoDocumento;
+
+	@OneToOne(fetch = FetchType.LAZY, orphanRemoval = true)
+	@JoinColumn(name = "foto_usuario", nullable = true)
+	private Foto fotoUsuario;
+
 }
