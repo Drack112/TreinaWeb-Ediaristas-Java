ec6406dc06df8b387cac669c07b0944b33da13ec
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-23 21:30:04 -0300

    :recycle: change cammelCase to snake_case

--- src/main/java/br/com/treinaweb/ediaristas/api/dto/responses/DiaristaLocalidadesPagedResponse.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/dto/responses/DiaristaLocalidadesPagedResponse.java
@@ -1,10 +1,14 @@
 package br.com.treinaweb.ediaristas.api.dto.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class DiaristaLocalidadesPagedResponse {
 	private List<DiaristaLocalidadeResponse> diaristas;
 
