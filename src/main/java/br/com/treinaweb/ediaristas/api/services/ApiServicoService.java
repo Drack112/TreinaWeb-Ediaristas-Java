b0d551c063fe3e4821aa942b7dd721098d0fc99d
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-24 10:29:56 -0300

    :camera_flash: refactor buscarTodos on ApiServicoService.java

--- src/main/java/br/com/treinaweb/ediaristas/api/services/ApiServicoService.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/services/ApiServicoService.java
@@ -5,9 +5,9 @@ import br.com.treinaweb.ediaristas.api.mappers.ApiServiceMapper;
 import br.com.treinaweb.ediaristas.core.models.Servico;
 import br.com.treinaweb.ediaristas.core.repositories.ServicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,14 +21,12 @@ public class ApiServicoService {
 
 
 	public List<ServicoResponse> buscarTodos() {
-		var servicos = repository.findAll();
-		var response = new ArrayList<ServicoResponse>();
+		var servicoSort = Sort.sort(Servico.class);
+		var ordenacao = servicoSort.by(Servico::getPosicao).ascending();
 
-		for (Servico servico : servicos) {
-			var servicoResponse = mapper.toResponse(servico);
-			response.add(servicoResponse);
-		}
-
-		return response;
+		return repository.findAll(ordenacao)
+			.stream()
+			.map(mapper::toResponse)
+			.toList();
 	}
 }
