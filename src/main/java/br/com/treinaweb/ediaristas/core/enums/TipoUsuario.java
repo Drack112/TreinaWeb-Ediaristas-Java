b66c2e88ed0cf62e6cd26640fdbf55e7a4779ca2
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-24 12:21:25 -0300

    :sparkles: add ID on enum TipoUsuario

--- src/main/java/br/com/treinaweb/ediaristas/core/enums/TipoUsuario.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/enums/TipoUsuario.java
@@ -1,7 +1,14 @@
 package br.com.treinaweb.ediaristas.core.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum TipoUsuario {
-    ADMIN,
-    CLIENTE,
-    DIARISTA;
+	ADMIN(3),
+	CLIENTE(1),
+	DIARISTA(2);
+
+	private Integer id;
 }
