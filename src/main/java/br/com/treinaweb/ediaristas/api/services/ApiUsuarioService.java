008ba9c12c0a5c1cce5640afad4d23e4c31fb410
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-02-24 17:41:08 -0300

    :lock: method to handle with password and password_confirmation

--- src/main/java/br/com/treinaweb/ediaristas/api/services/ApiUsuarioService.java
+++ src/main/java/br/com/treinaweb/ediaristas/api/services/ApiUsuarioService.java
@@ -3,11 +3,13 @@ package br.com.treinaweb.ediaristas.api.services;
 import br.com.treinaweb.ediaristas.api.dto.requests.UsuarioRequest;
 import br.com.treinaweb.ediaristas.api.dto.responses.UsuarioResponse;
 import br.com.treinaweb.ediaristas.api.mappers.ApiUsuarioMapper;
+import br.com.treinaweb.ediaristas.core.exceptions.SenhasNaoConferemException;
 import br.com.treinaweb.ediaristas.core.repositories.UsuarioRepository;
 import br.com.treinaweb.ediaristas.core.validators.UsuarioValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.FieldError;
 
 @Service
 public class ApiUsuarioService {
@@ -25,6 +27,9 @@ public class ApiUsuarioService {
 	private UsuarioValidator validator;
 
 	public UsuarioResponse cadastrar(UsuarioRequest request) {
+
+		validarConfirmacaoSenha(request);
+
 		var usuarioParaCadastrar = mapper.toModel(request);
 
 		validator.validar(usuarioParaCadastrar);
@@ -35,6 +40,18 @@ public class ApiUsuarioService {
 		var usuarioCadastrado = repository.save(usuarioParaCadastrar);
 
 		return mapper.toResponse(usuarioCadastrado);
+	}
+
+	private void validarConfirmacaoSenha(UsuarioRequest request) {
+		var senha = request.getPassword();
+		var confirmacaoSenha = request.getPasswordConfirmation();
+
+		if (!senha.equals(confirmacaoSenha)) {
+			var mensagem = "Os dois camps nao conferem";
+			var fieldError = new FieldError(request.getClass().getName(), "passwordConfirmation", request.getPasswordConfirmation(), false, null, null, mensagem);
+
+			throw new SenhasNaoConferemException(mensagem, fieldError);
+		}
 	}
 
 }
