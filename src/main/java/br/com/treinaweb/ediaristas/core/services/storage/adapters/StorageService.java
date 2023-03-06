d3729eaf29cee3d914f2fafc333085c66e9f09a7
Drack <onepessoa3@gmail.com>
2023-03-06 17:46:39 -0300

    :sparkles: implement buscarFoto on storageProvider

--- src/main/java/br/com/treinaweb/ediaristas/core/services/storage/adapters/StorageService.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/services/storage/adapters/StorageService.java
@@ -2,8 +2,11 @@ package br.com.treinaweb.ediaristas.core.services.storage.adapters;
 
 import br.com.treinaweb.ediaristas.core.models.Foto;
 import br.com.treinaweb.ediaristas.core.services.storage.exceptions.StorageServiceException;
+import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
 	Foto salvar(MultipartFile file) throws StorageServiceException;
+
+	UrlResource buscarFoto(String filename) throws StorageServiceException;
 }
--- src/main/java/br/com/treinaweb/ediaristas/core/services/storage/providers/LocalStorageProvider.java
+++ src/main/java/br/com/treinaweb/ediaristas/core/services/storage/providers/LocalStorageProvider.java
@@ -1,14 +1,18 @@
 package br.com.treinaweb.ediaristas.core.services.storage.providers;
 
+import br.com.treinaweb.ediaristas.api.controllers.StorageRestController;
 import br.com.treinaweb.ediaristas.core.models.Foto;
 import br.com.treinaweb.ediaristas.core.repositories.FotoRepository;
 import br.com.treinaweb.ediaristas.core.services.storage.adapters.StorageService;
 import br.com.treinaweb.ediaristas.core.services.storage.exceptions.StorageServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -30,6 +34,17 @@ public class LocalStorageProvider implements StorageService {
 		}
 	}
 
+	@Override
+	public UrlResource buscarFoto(String filename){
+		var arquivo = pastaUpload.resolve(filename);
+
+		try {
+			return new UrlResource(arquivo.toUri());
+		}catch (MalformedURLException e){
+			throw new StorageServiceException();
+		}
+	}
+
 	private Foto trySalvar(MultipartFile file) throws IOException {
 		if (!Files.exists(pastaUpload)) {
 			Files.createDirectories(pastaUpload);
@@ -40,13 +55,14 @@ public class LocalStorageProvider implements StorageService {
 		return fotoRepository.save(foto);
 	}
 
-	private Foto gerarModelFoto(MultipartFile file) {
+	private Foto gerarModelFoto(MultipartFile file) throws IOException {
 		var foto = new Foto();
 		var filename = gerarNovoFilename(file.getOriginalFilename());
+		var url = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(StorageRestController.class).buscarFoto(filename)).toString();
 		foto.setFilename(filename);
 		foto.setContentLength(file.getSize());
 		foto.setContentType(file.getContentType());
-		foto.setUrl("url da foto");
+		foto.setUrl(url);
 		return foto;
 	}
 
